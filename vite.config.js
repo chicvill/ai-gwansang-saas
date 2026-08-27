6f84b4e16c84990819a3640a8697bd45c81e3d4c
chicvill <chicvill@users.noreply.github.com>
2026-08-27 17:28:32 +0900

    chore: configure relative base path and add initial production index.html file

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   envDir: 'D:\\Workstation\\MQnet',
   plugins: [react()],
+
   server: {
     port: 5173,
     host: '0.0.0.0',
